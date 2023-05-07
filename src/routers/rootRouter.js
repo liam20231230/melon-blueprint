0093cd08db6bdeac0235dec710cfea43999e6399
liam20231230 <goguma2580852@gmail.com>
2023-05-07 17:04:30 +0900

    This is the first upload. I built a server and displayed hello world.

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { home } from "../controllers/songController";
+import globalRouter from "./globalRouter";
 
 const rootRouter = express.Router();
 
-rootRouter.get("/", home);
+rootRouter.use("/", globalRouter);
 
 export default rootRouter;
